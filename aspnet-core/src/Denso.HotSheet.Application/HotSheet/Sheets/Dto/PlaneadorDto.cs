f571fdad79ce8c1b2d88d8369279ff4f6f0eed9c
Ing.Luis Hdez Hdez <jrmc123lhh@gmail.com>
2026-04-15 07:12:40 -0600

    Actualizacion
    se agrega el catalogo de planeadores e dto

---
+++ aspnet-core/src/Denso.HotSheet.Application/HotSheet/Sheets/Dto/PlaneadorDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Denso.HotSheet.HotSheet.Sheets.Dto
+{
+    public class PlaneadorDto
+    {
+        public string Nombre { get; set; }
+        public List<string> Correos { get; set; }
+    }
+}
